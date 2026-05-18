be96d00a8a0910f3195880a0b8e82d4d65980ade
Raza Ali <apatniali@gmail.com>
2026-05-18 05:06:55 +0500

    Fix folders and update new UI

--- scratch.js
+++
@@ -1,9 +0,0 @@
-fetch('https://alwateen.net/list-of-items')
-  .then(r => r.text())
-  .then(t => {
-    const mp4Matches = t.match(/https?:\/\/[^\s"'><]+?\.mp4/gi);
-    const srcMatches = t.match(/src=["']([^"']+\.mp4)["']/gi);
-    console.log("MP4 URLs:", mp4Matches);
-    console.log("SRC Matches:", srcMatches);
-  })
-  .catch(e => console.error(e));
